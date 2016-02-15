1e1e6fa8bc73a6ec31d7f14c6c0f7a5ffc59eba6
Brad Butterfield <butters5789@gmail.com>
2016-02-15 14:59:58 -0700

    getElementsByClassName

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-console.log("sanity check")
+console.log("sanity check");
 
 var firstParagraph = document.getElementById('main');
 console.log(firstParagraph);
 
 var getAllElements = document.getElementsByTagName('ul');
-console.log(getAllElements)
+console.log(getAllElements);
 
 var getSingleElement = document.getElementsByTagName('ul')[0];
-console.log(getSingleElement)
+console.log(getSingleElement);
 
 var allListItems = document.getElementsByTagName('li');
 console.log(allListItems);
+
 for (var i = 0; i < allListItems.length; i++) {
   console.log(allListItems[i]);
 }
+
+var evenElements = document.getElementsByClassName('even');
+console.log(evenElements);
+
+var oddElements = document.getElementsByClassName('odd');
+console.log(oddElements);
+
+var firstEvenElement = document.getElementsByClassName('even')[0];
+console.log(firstEvenElement);
+
+var firstOddElement = document.getElementsByClassName('odd')[0];
+console.log(firstOddElement);
